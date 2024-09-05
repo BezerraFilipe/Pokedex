627103e6541fa07473c2bb71e6bc5a4d3bff2cb9
Filipe Bezerra <bezerrafilipe13@gmail.com>
2024-09-05 12:33:25 -0300

    i've learn how to use JS on web using some functions, in this commit, i used some methods like 'getElementById', or the object type 'document' to integrate JS and HTML

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -2,6 +2,33 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToHtml(pokemon){
+
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+
+
+                    <ol class="types">
+
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+    
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                    alt="${pokemon.name}">
+            
+                </div>
+            </li>
+            `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
 fetch(url) // in this case, the fetch method is trying to get a Json content on this URL, wich is a pokeAPI
 /*
 the fetch method wants a resource from internet using the promise/response idea
@@ -14,7 +41,16 @@ the promise is a type of object that will give you a resource once the response 
     .then((response) => response.json()) // a arrow function is a simpler way to describe one function that won't be used again; the left side of the arrow is the parameter of the funcion, and the right side of the arrow is the return;
     /*once that the promisse has been complete, we have the response;
     but this response is in a diferent format that what we want, so we use .json(), a method that translate the response to a Json format */
-    .then((bodyJson) => console.log(bodyJson))
+    .then((bodyJson) => bodyJson.results)
+    .then((pokemons) => {
+            for (i =0 ; i<pokemons.length; i++){
+                const pokemon = pokemons[i];
+                pokemonList.innerHTML += convertPokemonToHtml(pokemon)
+            }
+    
+    })
+
+    
     /*Here, the bodyJson represents the translated response of the fetch(URL) promisse; And then we just print that*/
     .catch((error) => console.error(error))
     /* the catch scope is what will be executed in case some error happends during de request*/
@@ -23,9 +59,4 @@ the promise is a type of object that will give you a resource once the response 
     })
     /*the finally scope is the part of the code that will be executed regardless the previous cases*/
 
-
-
-const x = 10+10
-console.log(x)
-
-/*here is just a test to see in the console that even the fetch() has been declared before this operations, this console.log will be printed before, because of the time waiting the reponse;*/
+    
