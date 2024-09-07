32fbc3f55ba5a040936a44a3ad26a12ce0f5e0e4
Filipe Bezerra <bezerrafilipe13@gmail.com>
2024-09-07 06:02:00 -0300

    making the pokemons info dinamic trough the url request

--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -3,13 +3,12 @@ const pokeApi = {}
 pokeApi.getPokemons = (offset = 0 , limit = 10) =>{
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-return fetch(url) 
+return fetch(url) // read more about Fetch API
         .then((response) => response.json()) 
         .then((bodyJson) => bodyJson.results)
+        .then((pokemons) => pokemons.map((pokemon)=> fetch(pokemon.url).then((pokeBody => pokeBody.json())))) 
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.error(error))
-
-    
 }
-
-
 
