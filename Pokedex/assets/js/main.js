06a0ab382b3c485a22a7d5713c3c9eb9baf41ae0
Filipe Bezerra <bezerrafilipe13@gmail.com>
2024-09-09 15:32:35 -0300

    adding a hover feature, but with some bugs

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,38 +1,58 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const limit = 12
-let offset = 0
+
+const limit = 12 //number of pokemons loaded per page
+let offset = 0 // the "index" of pokemons list
 
 const maxRecords = 151
 
+function loadInformations(){
 
-function convertPokemonToHtml(pokemon){
-    return `
-            <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-                <div class="detail">
-
-                    <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-
-                    <img src="${pokemon.photo}" 
-                    alt="${pokemon.name}">
-            
-                </div>
-            </li>`
 }
 
 function loadPokemons(offset, limit){
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {  
-        pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+        pokemonList.innerHTML += pokemons.map((pokemon) => `
+                <div id= "card-${pokemon.number}">
+                    <li class="pokemon ${pokemon.type}">
+                        <span class="number">#${pokemon.number}</span>
+                        <span class="name">${pokemon.name}</span>
+                        <div class="detail">
+                            <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                            </ol>
+                            <img src="${pokemon.photo}" alt="${pokemon.name}">
+                        </div>
+                    </li>
+                </div>
+                `).join('')
+    
+                
+        pokemons.forEach((pokemon) => {
+            const pokeCard = document.getElementById(`card-${pokemon.number}`)
+            pokeCard.addEventListener('mouseover', () => {
+                if (!document.getElementById(`${pokemon.number}Info`)){
+                    pokeCard.innerHTML += `<div id = "${pokemon.number}Info" class="pokeInfo" style="color: white;">${pokemon.name}_Informations_Here</div>`
+                }   
+            })
+            pokeCard.addEventListener('mouseout', () => {
+                
+                if(document.getElementById(`${pokemon.number}Info`)){
+                    pokeCard.removeChild(document.getElementById(`${pokemon.number}Info`))
+                }
+                
+            })
+        })
     })
+    
+    
 }
 
+// load the firts pokemons
 loadPokemons(offset, limit)
 
+// when clicking the button, more pokemons are added 
 loadMoreButton.addEventListener('click', ()=>{
 
     offset += limit
@@ -49,4 +69,6 @@ loadMoreButton.addEventListener('click', ()=>{
         loadPokemons(offset, limit)
     }
     
+
 })
+
